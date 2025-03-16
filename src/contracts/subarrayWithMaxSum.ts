810159b954ab91c7226bc5940c6a48a699781f64
Phoebe Manning <phoebe.manning8@gmail.com>
2025-03-16 12:05:38 -0700

    update subarrayWithMaxSum contract

--- src/contracts/subarrayWithMaxSum.ts
+++ src/contracts/subarrayWithMaxSum.ts
@@ -6,8 +6,8 @@
 // which has the largest sum and return that sum. 'Sum' refers to the sum of all the numbers in the 
 // subarray.
 // INPUT 1: -7,6,-6,5,6,-4,6,-1,1,3,-1
+
 // INPUT 2: -5,6,1,-10,-2,-5,-1,7,-10,6,-7,6,-1,9,4,-9,-5,9,-3
-// INPUT 3: -7,-6,-8,0,2,-2,-7,-5,8,-5,-1,-3,0,0,-2,0,-8,6
 
 // If your solution is an empty string, you must leave the text box empty. Do not use "", '', or ``.
 // NOTES: found on zer0 server (contract-590622.cct)
@@ -19,66 +19,45 @@ import { NS } from "@ns";
  * Output: void (prints sum)
  * @param ns 
  */
-export async function main(ns: NS, input: number[] | undefined) {
+export async function main(ns: NS): Promise<void> {
   let currentBestSubarray: number[] = [];
-  let array: number[] | undefined;
-  if (!input) {
-    array = [-8, -8, -9, 0, 8, 0, 10, -7, 2, 7, -5, -2, -4, -3, -8, -2, 1, 10, 5, -3, -8, 10, -5, -5, -8, 8, 9, -8];
+
+  const array = [-7,-6,-8,0,2,-2,-7,-5,8,-5,-1,-3,0,0,-2,0,-8,6];
+  let bestSum = -8; // choose the smallest numbe rin the array -> the answer must include one element
+
+  for (let i = 0; i < array.length; i++) {
+    const subarray = array.slice(i);
+    let curSum = subarray[0];
+    let curSubArray: number[] = [];
+
+    // check if first element in subarray is the best max subarray
+    if(curSum > bestSum) { curSubArray = [curSum]; bestSum = curSum } 
+    for (let j = 1; j < subarray.length; j++) {
+      if (curSum + subarray[j] > curSum) {
+        curSubArray = subarray.slice(0, j + 1) // slice j index is exclusive
+      }
+      curSum += subarray[j];
+      ns.tprint("curSum: ", curSum)
+
+      if (curSum >= bestSum) {
+        bestSum = curSum;
+        currentBestSubarray = curSubArray;
+        ns.tprint("new sum: ", bestSum);
+        ns.tprint("best subArr: ", currentBestSubarray);
+      }
+    }
   }
   array = input;
 
-  if (array) {
-    // find the smallest number in the array -> the answer must include one element
-    let indexOfSmallestNumber = 0;
-    let smallestNumber = array[0];
-    array.forEach((e, i) => {
-      if (e < smallestNumber) {
-        smallestNumber = e;
-        indexOfSmallestNumber = i
-      }
-    })
-    let bestSum = array[indexOfSmallestNumber];
-
-    for (let i = 0; i < array.length; i++) {
-      const subarray = array.slice(i);
-      let curSum = subarray[0];
-      let curSubArray: number[] = [];
-
-      // check if first element in subarray is the best max subarray
-      if (curSum > bestSum) {
-        curSubArray = [curSum];
-        bestSum = curSum;
-      }
-      for (let j = 1; j < subarray.length; j++) {
-        if (curSum + subarray[j] > curSum) {
-          curSubArray = subarray.slice(0, j + 1) // slice j index is exclusive
-        }
-        curSum += subarray[j];
-
-        if (curSum >= bestSum) {
-          bestSum = curSum;
-          currentBestSubarray = curSubArray;
-        }
-      }
-    }
-    // ns.tprint("SUM: ", bestSum);
-    // ns.tprint("SUBARRAY: ", currentBestSubarray)
-    return { bestSum, currentBestSubarray };
-  } else {
-    ns.tprint("issue reading input")
-  }
-  return;
+  ns.tprint("SUM: ", bestSum)
 }
 
 // ATTEMPTS:
 // 1. Input: -7,6,-6,5,6,-4,6,-1,1,3,-1, answer: [6,-6,5,6,-4,6,-1,1,3] -> 16
 
-// Input: -5,6,1,-10,-2,-5,-1,7,-10,6,-7,6,-1,9,4,-9,-5,9,-3,
+// Input: -5,6,1,-10,-2,-5,-1,7,-10,6,-7,6,-1,9,4,-9,-5,9,-3, 
 // 1. Answer: [6,-1,9,4,-9,-5,9] -> 13 INCORRECT
 // 2. Answer: [6,-1,9,4] -> 18 CORRECT
 
 // Input: -7,-6,-8,0,2,-2,-7,-5,8,-5,-1,-3,0,0,-2,0,-8,6
 // 1. Answer: [8] -> 8 CORRECT
-
-// Input: -8,-8,-9,0,8,0,10,-7,2,7,-5,-2,-4,-3,-8,-2,1,10,5,-3,-8,10,-5,-5,-8,8,9,-8
-// 1. Answer: 20 -> CORRECT
