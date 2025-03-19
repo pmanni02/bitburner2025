248ea076e77d030c89072215ea081e1d556e71c8
Phoebe Manning <phoebe.manning8@gmail.com>
2025-03-19 12:28:10 -0700

    add initial descriptions

---
+++ src/contracts/algorithmicStockTraderII.ts
@@ -0,0 +1,22 @@
+// Algorithmic Stock Trader II
+// You are attempting to solve a Coding Contract. You have 10 tries remaining, after which the 
+// contract will self-destruct.
+
+// You are given the following array of stock prices (which are numbers) where the i-th element 
+// represents the stock price on day i:
+
+// 191,27,7,14,40,176,15,74,157,111,119,115,77,27,85,178,194,177,122,68,36,48,103,67,75,195,35,146,52
+
+// Determine the maximum possible profit you can earn using as many transactions as you'd like. 
+// A transaction is defined as buying and then selling one share of the stock. Note that you cannot 
+// engage in multiple transactions at once. In other words, you must sell the stock before you buy it again.
+
+// If no profit can be made, then the answer should be 0.
+
+// If your solution is an empty string, you must leave the text box empty. Do not use "", '', or ``.
+
+import { NS } from "@ns";
+
+export async function main(ns: NS): Promise<void> {
+  //
+}
