1ae13b1cadcd8f6e74e3944397cf82c822104ab2
Phoebe Manning <phoebe.manning8@gmail.com>
2025-03-19 16:47:06 -0700

    add spiralizeMatrix contract

---
+++ src/contracts/spiralizeMatrix.ts
@@ -0,0 +1,39 @@
+// Spiralize Matrix
+// You are attempting to solve a Coding Contract. You have 10 tries remaining, after
+//  which the contract will self-destruct.
+
+
+// Given the following array of arrays of numbers representing a 2D matrix, 
+//  return the elements of the matrix as an array in spiral order:
+
+//     [
+//         [37, 4,19,49,42,36,18,21,28]
+//         [26, 1,15,16, 7,30,38, 4, 5]
+//     ]
+
+// Here is an example of what spiral order should be:
+
+//     [
+//         [1, 2, 3]
+//         [4, 5, 6]
+//         [7, 8, 9]
+//     ]
+// Answer: [1, 2, 3, 6, 9, 8 ,7, 4, 5]
+
+// Note that the matrix will not always be square:
+
+//     [
+//         [1,  2,  3,  4]
+//         [5,  6,  7,  8]
+//         [9, 10, 11, 12]
+//     ]
+// Answer: [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+
+// If your solution is an empty string, you must leave the text box empty. Do not use "", '', or ``.
+
+import { NS } from "@ns";
+
+export async function main(ns: NS): Promise<void> {
+  ns.tprint("Hello Remote API!");
+}
+
