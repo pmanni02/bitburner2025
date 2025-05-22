2a67e400f0956d1234b25cca0ea31975307eea44
Phoebe Manning <phoebe.manning8@gmail.com>
2025-05-22 12:38:07 -0700

    wip - contract updates

---
+++ src/contracts/generateIPAddresses.ts
@@ -0,0 +1,27 @@
+// Generate IP Addresses
+// You are attempting to solve a Coding Contract. You have 10 tries remaining, 
+// after which the contract will self-destruct.
+
+// Given the following string containing only digits, return an array with all possible 
+// valid IP address combinations that can be created from the string:
+
+// 18214757196
+
+// Note that an octet cannot begin with a '0' unless the number itself is exactly '0'. 
+// For example, '192.168.010.1' is not a valid IP.
+
+// Examples:
+
+// 25525511135 -> ["255.255.11.135", "255.255.111.35"]
+// 1938718066 -> ["193.87.180.66"]
+// 1111 -> ["1.1.1.1"]
+
+// If your solution is an empty string, you must leave the text box empty. Do not use "", '', or ``.
+
+import { NS } from "@ns";
+
+export async function main(ns: NS): Promise<void> {
+  //
+}
+
+
