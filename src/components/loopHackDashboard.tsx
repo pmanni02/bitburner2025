7e127a03c69427641dab9217ddc6469933a27a17
Phoebe Manning <phoebe.manning8@gmail.com>
2025-06-04 15:18:36 -0700

    utilize react state for config updates

--- src/components/loopHackDashboard.tsx
+++ src/components/loopHackDashboard.tsx
@@ -1,13 +1,21 @@
 import { NS } from "@ns";
 import { LoopHackConfig } from "../interfaces";
-import { readServerConfig, writeServerConfig } from "../helpers";
+import { readServerConfig, writeServerConfig } from "../utils/helpers";
 import { BASIC_SCRIPT_RAM_SIZE } from "../constants";
 import { addNewServer, buyNewServer, changeTargetServer, serverPrompt, upgradePurchasedServer } from "/utils/servers";
 
 const myWindow = eval("window") as Window & typeof globalThis;
 const React = myWindow.React;
 
-export const LoopHackDashBoard = async function getHackUI(ns: NS, config: LoopHackConfig) {
+type Props = {
+  ns: NS,
+  config: LoopHackConfig
+}
+
+// export function LoopHackDashBoard(ns: NS, config: LoopHackConfig){
+export function LoopHackDashBoard({ ns, config }: Props) {
+  const [currentConfig, setCurrentConfig] = React.useState<LoopHackConfig>(config)
+
   return (
     <html>
       <head>
@@ -15,62 +23,112 @@ export const LoopHackDashBoard = async function getHackUI(ns: NS, config: LoopHa
       </head>
       <body>
         <div id="hack-div">
-          {addButton("Add Hack", "addHack", () => replaceScript(ns, "/utils/grow.js", "/utils/hack.js", config.targetServer))}
+          {/* {addButton("Add Hack", "addHack", () => replaceScript(ns, "/utils/grow.js", "/utils/hack.js", config.targetServer))} */}
+          <button id="addHack" onClick={() => {
+            const updatedConfig = replaceScript(ns, "/utils/grow.js", "/utils/hack.js", config.targetServer);
+            setCurrentConfig(updatedConfig)
+          }}>
+            Add Hack
+          </button>
           <details open>
             {/* <summary>Hack Servers: {[numHackThreads]}</summary> */}
             <summary>Hack Servers: </summary>
-            {await makeList(ns, config.hackServers)}
+            {makeList(ns, config.hackServers, currentConfig)}
           </details>
         </div>
 
         <div id="grow-div">
-          {addButton("Add Grow", "addGrow", () => replaceScript(ns, "/utils/hack.js", "/utils/grow.js", config.targetServer))}
+          {/* {addButton("Add Grow", "addGrow", () => replaceScript(ns, "/utils/hack.js", "/utils/grow.js", config.targetServer))} */}
+          <button id="addGrow" onClick={() => {
+            const updatedConfig = replaceScript(ns, "/utils/hack.js", "/utils/grow.js", config.targetServer);
+            setCurrentConfig(updatedConfig)
+          }}>
+            Add Grow
+          </button>
           <details open>
             {/* <summary>Grow Servers: {[numGrowThreads]}</summary> */}
             <summary>Grow Servers: </summary>
-            {await makeList(ns, config.growServers)}
+            {makeList(ns, config.growServers, currentConfig)}
           </details>
         </div>
 
         <div id="weaken-div">
-          {addButton("Add Weaken", "addWeaken", () => replaceScript(ns, "/utils/grow.js", "/utils/weaken.js", config.targetServer))}
-          {addButton("Remove Weaken", "removeWeaken", () => replaceScript(ns, "/utils/weaken.js", "/utils/grow.js", config.targetServer))}
+          {/* {addButton("Add Weaken", "addWeaken", () => replaceScript(ns, "/utils/grow.js", "/utils/weaken.js", config.targetServer))} */}
+          <button id="addWeaken" onClick={() => {
+            const updatedConfig = replaceScript(ns, "/utils/grow.js", "/utils/weaken.js", config.targetServer);
+            setCurrentConfig(updatedConfig)
+          }}>
+            Add Weaken
+          </button>
+
+          {/* {addButton("Remove Weaken", "removeWeaken", () => replaceScript(ns, "/utils/weaken.js", "/utils/grow.js", config.targetServer))} */}
+          <button id="removeWeaken" onClick={() => {
+            const updatedConfig = replaceScript(ns, "/utils/weaken.js", "/utils/grow.js", config.targetServer);
+            setCurrentConfig(updatedConfig)
+          }}>
+            Remove Weaken
+          </button>
+
           <details open>
             {/* <summary>Weaken Servers: {[numWeakenThreads]}</summary> */}
             <summary>Weaken Servers: </summary>
-            {await makeList(ns, config.weakenServers)}
+            {makeList(ns, config.weakenServers, currentConfig)}
           </details>
         </div>
 
         <br></br>
 
-        {addButton("Buy Server", "buyServer", () => buyNewServer(ns))}
-        {addButton("Upgrade Server", "upgradeServer", () => upgradePurchasedServer(ns))}
+        {/* {addButton("Buy Server", "buyServer", () => buyNewServer(ns))} */}
+        <button id="buyServer" onClick={async () => {
+          const updatedConfig = await buyNewServer(ns, currentConfig)
+          setCurrentConfig(updatedConfig)
+        }}>
+          Buy Server
+        </button>
+
+
+        {/* {addButton("Upgrade Server", "upgradeServer", () => upgradePurchasedServer(ns))} */}
+        <button id="upgradeServer" onClick={async () => {
+          const updatedConfig = await upgradePurchasedServer(ns, currentConfig)
+          setCurrentConfig(updatedConfig)
+        }}>
+          Upgrade Server
+        </button>
 
         <br></br>
-        {addButton("Add Server", "addServer", () => addNewServer(ns))}
-        {addButton("Change Target", "changeTarget", async () => await changeTargetServer(ns))}
+        {/* {addButton("Add Server", "addServer", () => addNewServer(ns))} */}
+        <button id="addServer" onClick={async () => {
+          const updatedConfig = await addNewServer(ns, currentConfig)
+          if (updatedConfig) {
+            setCurrentConfig(updatedConfig)
+          }
+        }}>
+          Add Server
+        </button>
+
+
+        {/* {addButton("Change Target", "changeTarget", async () => await changeTargetServer(ns))} */}
+        <button id="changeTarget" onClick={async () => {
+          const updatedConfig = await changeTargetServer(ns, currentConfig)
+          if (updatedConfig) {
+            setCurrentConfig(updatedConfig)
+          }
+        }}>
+          Change Target
+        </button>
       </body>
     </html>
   )
 }
 
-function addButton(buttonName: string, buttonId: string, onClickFn: () => void) {
-  return (
-    <button id={buttonId} onClick={onClickFn}>
-      {buttonName}
-    </button>
-  );
-}
-
-async function makeList(ns: NS, array: string[]) {
+function makeList(ns: NS, array: string[], config: LoopHackConfig) {
   const listItems = array.map((item) => (
-    <p key={item} onClick={() => serverPrompt(ns, item)}> {item}</p>
+    <p key={item} onClick={() => serverPrompt(ns, item, config)}> {item}</p>
   ));
   return listItems;
 }
 
-export const replaceScript = (ns: NS, scriptToKill: string, scriptToStart: string, targetServer: string) => {
+export const replaceScript = (ns: NS, scriptToKill: string, scriptToStart: string, targetServer: string): LoopHackConfig => {
   const config: LoopHackConfig = readServerConfig(ns)[0];
 
   let serverName;
@@ -111,4 +169,5 @@ export const replaceScript = (ns: NS, scriptToKill: string, scriptToStart: strin
     writeServerConfig(ns, config);
     // updateGlobalNumThreads(numThreads, scriptToStart)
   }
+  return config
 }
