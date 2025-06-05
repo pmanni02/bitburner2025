214a326295e6738a34fa37cad7ded4706dd9719f
Phoebe Manning <phoebe.manning8@gmail.com>
2025-06-04 20:53:30 -0700

    setup initial testing

---
+++ tests/index.test.js
@@ -0,0 +1,17 @@
+// import { NS } from "../NetscriptDefinitions.d.ts"
+// import { addNewServer } from '../src/utils/servers.ts'
+
+describe('Server helper functions', () => {
+  describe('addNewServer', () => {
+    test('it should add a new server to an existing config', () => {
+      // const config = {
+      //   growServers: ["n00dles", "sigma-cosmetics"],
+      //   hackServers: [],
+      //   weakenServers: ["joesguns"],
+      //   targetServer: "foodnstuff"
+      // }
+      // const updatedConfig = addNewServer(NS, config)
+      expect(1).toBe(1)
+    })
+  })
+})
