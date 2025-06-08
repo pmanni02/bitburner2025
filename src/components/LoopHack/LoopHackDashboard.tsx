fe6f9f6202471def67a8584030599a620625902e
Phoebe Manning <phoebe.manning8@gmail.com>
2025-06-08 13:13:55 -0700

    add components for loop hack dashboard

--- src/components/LoopHack/LoopHackDashboard.tsx
+++ src/components/LoopHack/LoopHackDashboard.tsx
@@ -1,7 +1,9 @@
 import { NS } from "@ns";
 import { LoopHackConfig } from "../../interfaces";
-import { addNewServer, buyNewServer, changeTargetServer, replaceScript, serverPrompt, upgradePurchasedServer } from "/utils/servers";
+import { addNewServer, buyNewServer, changeTargetServer, replaceScript, upgradePurchasedServer } from "/utils/servers";
 import { growScriptPath, hackScriptPath, weakenScriptPath } from "/constants";
+import { Button } from "./Button";
+import { ServerDetails } from "./ServerDetails";
 
 const myWindow = eval("window") as Window & typeof globalThis;
 const React = myWindow.React;
@@ -20,95 +22,77 @@ export function LoopHackDashboard({ ns, config }: Props) {
         <meta charSet="utf-8"></meta>
       </head>
       <body>
-        <div id="hack-div">
-          <button id="addHack" onClick={() => {
-            const updatedConfig = replaceScript(ns, growScriptPath, hackScriptPath, config);
-            setCurrentConfig(updatedConfig)
-          }}>
-            Add Hack
-          </button>
-          <details open>
-            <summary>Hack Servers: </summary>
-            {makeList(ns, config.hackServers, currentConfig)}
-          </details>
+        <div id="hack">
+          <Button id='addHack' name='Add Hack' onClickFn={
+            () => {
+              const updatedConfig = replaceScript(ns, growScriptPath, hackScriptPath, config);
+              setCurrentConfig(updatedConfig)
+            }}
+          />
+          <ServerDetails ns={ns} config={config} serverNames={currentConfig.hackServers} summary="Hack Servers: " />
         </div>
 
-        <div id="grow-div">
-          <button id="addGrow" onClick={() => {
-            const updatedConfig = replaceScript(ns, hackScriptPath, growScriptPath, config);
-            setCurrentConfig(updatedConfig)
-          }}>
-            Add Grow
-          </button>
-          <details open>
-            <summary>Grow Servers: </summary>
-            {makeList(ns, config.growServers, currentConfig)}
-          </details>
+        <div id="grow">
+          <Button id='addGrow' name='Add Grow' onClickFn={
+            () => {
+              const updatedConfig = replaceScript(ns, hackScriptPath, growScriptPath, config);
+              setCurrentConfig(updatedConfig)
+            }
+          }
+          />
+          <ServerDetails ns={ns} config={config} serverNames={currentConfig.growServers} summary="Grow Servers: " />
         </div>
 
-        <div id="weaken-div">
-          <button id="addWeaken" onClick={() => {
-            const updatedConfig = replaceScript(ns, growScriptPath, weakenScriptPath, config);
-            setCurrentConfig(updatedConfig)
-          }}>
-            Add Weaken
-          </button>
-
-          <button id="removeWeaken" onClick={() => {
-            const updatedConfig = replaceScript(ns, weakenScriptPath, growScriptPath, config);
-            setCurrentConfig(updatedConfig)
-          }}>
-            Remove Weaken
-          </button>
-
-          <details open>
-            <summary>Weaken Servers: </summary>
-            {makeList(ns, config.weakenServers, currentConfig)}
-          </details>
+        <div id="weaken">
+          <Button id='addWeaken' name='Add Weaken' onClickFn={
+            () => {
+              const updatedConfig = replaceScript(ns, growScriptPath, weakenScriptPath, config);
+              setCurrentConfig(updatedConfig)
+            }
+          } />
+          <Button id='removeWeaken' name='Remove Weaken' onClickFn={
+            () => {
+              const updatedConfig = replaceScript(ns, weakenScriptPath, growScriptPath, config);
+              setCurrentConfig(updatedConfig)
+            }
+          } />
+          <ServerDetails ns={ns} config={config} serverNames={currentConfig.weakenServers} summary="Weaken Servers: " />
         </div>
 
         <br></br>
 
-        <button id="buyServer" onClick={async () => {
-          const updatedConfig = await buyNewServer(ns, currentConfig)
-          setCurrentConfig(updatedConfig)
-        }}>
-          Buy Server
-        </button>
-
-        <button id="upgradeServer" onClick={async () => {
-          const updatedConfig = await upgradePurchasedServer(ns, currentConfig)
-          setCurrentConfig(updatedConfig)
-        }}>
-          Upgrade Server
-        </button>
+        <Button id='buyServer' name='Buy Server' onClickFn={
+          async () => {
+            const updatedConfig = await buyNewServer(ns, currentConfig)
+            setCurrentConfig(updatedConfig)
+          }
+        } />
+        <Button id='upgradeServer' name='Upgrade Server' onClickFn={
+          async () => {
+            const updatedConfig = await upgradePurchasedServer(ns, currentConfig)
+            setCurrentConfig(updatedConfig)
+          }
+        } />
 
         <br></br>
-        <button id="addServer" onClick={async () => {
-          const updatedConfig = await addNewServer(ns, currentConfig)
-          if (updatedConfig) {
-            setCurrentConfig(updatedConfig)
+        <Button id='addServer' name='Add Server' onClickFn={
+          async () => {
+            const updatedConfig = await addNewServer(ns, currentConfig)
+            if (updatedConfig) {
+              setCurrentConfig(updatedConfig)
+            }
           }
-        }}>
-          Add Server
-        </button>
+        } />
 
-        <button id="changeTarget" onClick={async () => {
-          const updatedConfig = await changeTargetServer(ns, currentConfig)
-          if (updatedConfig) {
-            setCurrentConfig(updatedConfig)
+        <Button id='changeTarget' name='Change Target' onClickFn={
+          async () => {
+            const updatedConfig = await changeTargetServer(ns, currentConfig)
+            if (updatedConfig) {
+              setCurrentConfig(updatedConfig)
+            }
           }
-        }}>
-          Change Target
-        </button>
+        } />
       </body>
     </html>
   )
-}
-
-function makeList(ns: NS, array: string[], config: LoopHackConfig) {
-  const listItems = array.map((item) => (
-    <p key={item} onClick={() => serverPrompt(ns, item, config)}> {item}</p>
-  ));
-  return listItems;
 }
